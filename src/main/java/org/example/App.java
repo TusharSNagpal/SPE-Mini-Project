9ba3e15c06f693d1e3a7fa2c579b46b489b37dc1
TusharSNagpal <nagpal8795@gmail.com>
2023-03-11 18:38:18 +0530

    Dockerfile-rename

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!" );
+        double number1, number2;
+//        int op;
+//        op = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.print("Enter Number 1: ");
+            number1 = scanner.nextDouble();
+            System.out.println();
+            System.out.print("Enter Number 2: ");
+            number2 = scanner.nextDouble();
+            System.out.println();
+
+            double ans = pow(number1, number2);
+            System.out.println(ans);
+        }
     }
 }
